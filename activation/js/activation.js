ceb6485e10cc27cf63b0fa19a84823ee768282a7
JiangJhong <happygo0505@gmail.com>
2023-11-11 22:22:35 +0800

    a

--- activation/js/activation.js
+++ activation/js/activation.js
@@ -1,61 +1,11 @@
 // JavaScript to handle form submission and validation
 document.getElementById('uscc-ctn').addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent default form submission behavior
-    const emailField = document.getElementById('email-input');
-    const emailError = document.getElementById('email-error');
-    const serverError = document.getElementById('server-error');
-    const email = emailField.value;
-    const form = document.getElementById('uscc-ctn');
-  
-    // Validate email
-    if (!email || !email.includes('@')) {
-      // Display error message and apply styles to the input field
-      emailError.style.display = 'block';
-      emailField.style.borderColor = 'red';
-      emailField.focus();
-      return;
-    } else {
-      // Hide error message and reset styles
-      emailError.style.display = 'none';
-    }
-  
-    // Get the 'code' parameter from the URL
-    const code = getURLParameter('code');
-  
-    // Submit the form
-    try {
-      const response = await fetch('https://app.marq.com/users/register/redeemCode', {
-        method: 'POST',
-        redirect: 'follow',
-        credentials: 'include',
-        body: JSON.stringify({ email, redemptionCode: code }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        // Include the 'code' parameter in the redirect URL
-        if (data.redirect.startsWith("http")) {
-          window.location.href = data.redirect;
-        } else {
-          const redirect = 'https://app.marq.com' + data.redirect + '?code=' + encodeURIComponent(code);
-          window.location.href = redirect;
-        }
-      } else {
-        // Handle server errors
-        const data = await response.json();
-        serverError.textContent = data.errorMessage;
-        serverError.classList.add('error-message');
-        serverError.style.display = 'block';
-      }
-    } catch (error) {
-      // Handle network errors
-      serverError.textContent = 'Network error: ' + error;
-      serverError.classList.add('error-message');
-      serverError.style.display = 'block';
-    }
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LePwQspAAAAAEacgyVDupWz-Vhs6rzxmhC7ZJAI', {action: 'submit'}).then(function(token) {
+            alert(token);
+        });
+    });
   });
   
   function getURLParameter(name) {
